887ea0aa809c08ae55abdb76ec5a70b3e13f675f
khyu2 <ikeelee7742@gmail.com>
2025-01-03 00:29:00 +0900

    feat: User API 구현
    - 인증 핸들러 구현
    - User API 구현

--- user-service/src/main/java/project/userservice/domain/service/UserService.java
+++ user-service/src/main/java/project/userservice/domain/service/UserService.java
@@ -1,20 +1,12 @@
 package project.userservice.domain.service;
 
-import project.userservice.domain.dto.request.UserRenewPasswordRequest;
-import project.userservice.domain.dto.request.UserSignUpRequest;
 import project.userservice.domain.dto.request.UserUpdateRequest;
 import project.userservice.domain.dto.response.UserInfoResponse;
-import project.userservice.domain.dto.response.UserSignUpResponse;
 
 public interface UserService {
 
-    UserSignUpResponse register(UserSignUpRequest request);
-
     UserInfoResponse getUserInfo(Long userId);
 
-    void updateUserNickname(Long userId, UserUpdateRequest request);
+    UserInfoResponse updateUserNickname(Long userId, UserUpdateRequest request);
 
-    void updateUserPassword(Long userId, UserRenewPasswordRequest request);
-
-    void withdrawUser(Long userId);
 }
