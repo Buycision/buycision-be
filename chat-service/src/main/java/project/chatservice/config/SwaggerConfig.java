fa6cf663e753bba342889cc79c9d9c634d6fa22f
sonsumin <sonsumin00@gmail.com>
2025-01-16 12:01:17 +0900

    Feat: Sheet code for chat

--- chat-service/src/main/java/project/chatservice/config/SwaggerConfig.java
+++ chat-service/src/main/java/project/chatservice/config/SwaggerConfig.java
@@ -3,11 +3,13 @@ package project.chatservice.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@Component("chatSwaggerConfig")
 public class SwaggerConfig {
 
-    @Bean
+    @Bean(name = "chatCustomOpenAPI")
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new io.swagger.v3.oas.models.info.Info()
--- user-service/src/main/java/project/userservice/config/SwaggerConfig.java
+++ user-service/src/main/java/project/userservice/config/SwaggerConfig.java
@@ -3,11 +3,13 @@ package project.userservice.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@Component("userSwaggerConfig")
 public class SwaggerConfig {
 
-    @Bean
+    @Bean(name = "userCustomOpenAPI")
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new io.swagger.v3.oas.models.info.Info()
