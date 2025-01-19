b8e4061e3bb7de25c39a418570247041b1a94e1e
HyeonSeong <gus8131@naver.com>
2025-01-19 11:43:49 +0900

    Fix : CalenderApi - 기능 구현

---
+++ community-service/src/main/java/project/communityservice/domain/calender/dto/request/CalenderRequest.java
@@ -0,0 +1,6 @@
+package project.communityservice.domain.calender.dto.request;
+
+public record CalenderRequest (
+        String title,
+        String content
+){ }
--- community-service/src/main/java/project/communityservice/domain/calender/service/CalenderService.java
+++ community-service/src/main/java/project/communityservice/domain/calender/service/CalenderService.java
@@ -1,17 +1,21 @@
 package project.communityservice.domain.calender.service;
 
-import project.communityservice.domain.calender.dto.CalenderRequest;
-import project.communityservice.domain.calender.dto.CalenderResponse;
+import project.communityservice.domain.calender.dto.response.CalenderResponse;
+import project.communityservice.domain.calender.dto.request.CalenderRequest;
+
+import java.util.List;
 
 public interface CalenderService {
+    List<CalenderResponse> listCalender();
+
     // 일정 조회
-    CalenderResponse getCalender(Long calenderId);
+    CalenderResponse getCalender(Long id);
 
     // 일정 등록
-    CalenderResponse addCalender(CalenderRequest calenderRequest);
+    CalenderResponse createCalender();
 
     // 일정 수정
-    CalenderResponse updateCalender(CalenderRequest calenderRequest);
+    CalenderResponse modifyCalender(CalenderRequest calenderRequest);
 
     // 일정 삭제
     void deleteCalender(Long calenderId);
