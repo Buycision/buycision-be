19269281f69128ac4df5c603c884bc650c079a29
sonsumin <sonsumin00@gmail.com>
2025-01-19 22:34:12 +0900

    Feat: beanException 해결

--- chat-service/src/main/java/project/chatservice/config/SwaggerConfig.java
+++ chat-service/src/main/java/project/chatservice/config/SwaggerConfig.java
@@ -3,10 +3,12 @@ package project.chatservice.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Configuration
 @Component("chatSwaggerConfig")
+@Profile("chat-service") //이 프로파일이 활성화될 때만 로드
 public class SwaggerConfig {
 
     @Bean(name = "chatCustomOpenAPI")
