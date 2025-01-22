7bdee3a64ce740f5b4805977eae2751ed15b4595
sonsumin <sonsumin00@gmail.com>
2025-01-22 14:11:28 +0900

    Feat: STOMP 연결

--- chat-service/src/main/java/project/chatservice/handler/StompHandler.java
+++ chat-service/src/main/java/project/chatservice/handler/StompHandler.java
@@ -1,38 +1,28 @@
-//package project.chatservice.handler;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.messaging.Message;
-//import org.springframework.messaging.MessageChannel;
-//import org.springframework.messaging.simp.stomp.StompCommand;
-//import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-//import org.springframework.messaging.support.ChannelInterceptor;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@RequiredArgsConstructor
-//@Slf4j
-//public class StompHandler implements ChannelInterceptor {
-//
-//    /**
-//     * WebSocket 메세지가 클라이언트와 서버 간에 전송되기 전에 가로채어 JWT인증을 처리
-//     * WebSocket 연결 과정에서 STOMP 헤더를 분석하여 토큰 유효성을 검사
-//     * 일단 HTTP API와 통신 방식이 다르기 때문에 security에서 처리하는 단방향이 아닌,
-//     * 양방향 통신 및 상태 유지 연결을 위해 JWT 인증을 가로채어 처리
-//     * @param message
-//     * @param channel
-//     * @return
-//     */
-//    @Override
-//    public Message<?> preSend(Message<?> message, MessageChannel channel) {
-//        try {
-//            StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-//            log.info("Received WebSocket message: {}", message.getHeaders());
-//            // 필요한 경우 추가 처리 로직
-//        } catch (Exception e) {
-//            log.error("WebSocket 핸들링 중 오류 발생: ", e);
-//            throw e; // 예외를 처리하고 로그를 남김
-//        }
-//        return message;
-//    }
-//}
+package project.chatservice.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StompHandler implements ChannelInterceptor {
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        try {
+            StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+            log.info("Received WebSocket message: {}", message.getHeaders());
+            // JWT 인증 로직 추가 가능
+        } catch (Exception e) {
+            log.error("WebSocket 핸들링 중 오류 발생: ", e);
+            throw e;
+        }
+        return message;
+    }
+}
