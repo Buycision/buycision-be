0b374b1703216120b5992ac177471f7c13bc24ae
sonsumin <sonsumin00@gmail.com>
2025-01-22 14:10:42 +0900

    Feat: 채팅방 자동 생성 로직

--- chat-service/src/main/java/project/chatservice/domain/service/ChatService.java
+++ chat-service/src/main/java/project/chatservice/domain/service/ChatService.java
@@ -1,10 +1,10 @@
 package project.chatservice.domain.service;
 
 import project.chatservice.domain.dto.request.MessageRequest;
+import project.chatservice.domain.dto.response.ChatResponseDto;
 import project.chatservice.domain.dto.response.MessageResponse;
 
 public interface ChatService {
-
     MessageResponse processMessage(MessageRequest request, String sessionId, String nickname);
-
+    ChatResponseDto getMessages(String authorizationHeader, Long roomId, int size, int page);
 }
--- chat-service/src/main/java/project/chatservice/handler/WebSocketHandler.java
+++ chat-service/src/main/java/project/chatservice/handler/WebSocketHandler.java
@@ -15,31 +15,19 @@ import project.chatservice.domain.entity.MessageType;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketHandler {
-    /**
-     * 메시지를 클래스 내에서 직접 SimpleBroker로 발행하는 데 사용됩니다.
-     */
+
     private final SimpMessagingTemplate messagingTemplate;
 
-    /**
-     * STOMP 서브 프로토콜을 통한 CONNECT 메시지가 수신되면 발생하는 이벤트 {@link SessionConnectEvent}를 처리합니다.
-     * <ul>
-     *     <li>STOMP CONNECT 메시지 헤더에서 nickname을 추출해 웹소켓 세션 헤더에 저장합니다.</li>
-     *     <li>사용자가 연결되었음을 알리는 메시지를 발행합니다.</li>
-     * </ul>
-     */
     @EventListener
     public void handleSessionConnect(SessionConnectEvent event) {
         SimpMessageHeaderAccessor accessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
         String nickname = accessor.getFirstNativeHeader("nickname");
 
-        // 세션에 닉네임 저장
         accessor.getSessionAttributes().put("nickname", nickname);
 
-        // 디버깅을 위해 sessionId와 nickname 을 함께 보여줍니다.
         String sessionId = accessor.getSessionId();
         log.info("[SessionConnected]: sessionId = " + sessionId + ", nickname = " + nickname);
 
-        // 시스템 메시지를 생성하고 발행합니다.
         MessageResponse responseDto = MessageResponse.builder()
                 .type(MessageType.SYSTEM)
                 .content(nickname + "님이 참가하였습니다.")
@@ -48,22 +36,14 @@ public class WebSocketHandler {
         messagingTemplate.convertAndSend("/topic/chat", responseDto);
     }
 
-    /**
-     * STOMP 서브 프로토콜을 사용하는 웹소켓 세션의 연결이 끊기면 발생하는 이벤트 {@link SessionDisconnectEvent}를 처리합니다.
-     * <ul>
-     *     <li>사용자가 연결 해제되었음을 알리는 메시지를 발행합니다.</li>
-     * </ul>
-     */
     @EventListener
     public void handleSessionDisconnect(SessionDisconnectEvent event) {
         SimpMessageHeaderAccessor accessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
         String nickname = (String) accessor.getSessionAttributes().get("nickname");
 
-        // 디버깅을 위해 sessionId와 nickname 을 함께 보여줍니다.
         String sessionId = accessor.getSessionId();
         log.info("[SessionDisconnected]: sessionId = " + sessionId + ", nickname = " + nickname);
 
-        // 시스템 메시지를 생성하고 발행합니다.
         MessageResponse responseDto = MessageResponse.builder()
                 .type(MessageType.SYSTEM)
                 .content(nickname + "님이 나갔습니다.")
