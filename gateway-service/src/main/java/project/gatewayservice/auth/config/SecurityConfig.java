e2e6ecd3c6d7b40f474893fbec8f859c52340c01
sonsumin <sonsumin00@gmail.com>
2025-01-22 14:11:48 +0900

    Feat: h2 경로 추가

--- gateway-service/src/main/java/project/gatewayservice/auth/config/SecurityConfig.java
+++ gateway-service/src/main/java/project/gatewayservice/auth/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         .pathMatchers("/v3/api-docs/**", "/user/v3/api-docs", "/chat/v3/api-docs", "/community/v3/api-docs").permitAll()
                         .pathMatchers("/user/email/**", "/user/register").permitAll() // for Auth
                         .pathMatchers("/chat/**").permitAll()
+                        .pathMatchers("/h2-console/**").permitAll()
                         .anyExchange().permitAll()
                 )
                 .logout(logout -> logout.logoutUrl("/logout"))
