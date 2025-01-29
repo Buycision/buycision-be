b1ca28b14f5c2fbf9641095cc5c12bf4aed809c9
HyeonSeong <gus8131@naver.com>
2025-01-29 12:15:06 +0900

    fix : 커뮤니티 API 파일 수정

--- community-service/src/main/java/project/communityservice/domain/calender/dto/request/CalenderRequest.java
+++
@@ -1,6 +0,0 @@
-package project.communityservice.domain.calender.dto.request;
-
-public record CalenderRequest (
-        String title,
-        String content
-){ }
--- community-service/src/main/java/project/communityservice/domain/community/dto/request/CommunityCreateRequest.java
+++
@@ -1,7 +0,0 @@
-package project.communityservice.domain.community.dto.request;
-
-public record CommunityCreateRequest(
-        String name,
-        String description
-        ) {
-}
