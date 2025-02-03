1bac8bcf9d17a8567588b7808bd319a2e3a70a2a
HyeonSeong <gus8131@naver.com>
2025-02-03 15:00:09 +0900

    feat : QueryDSL

--- community-service/src/main/java/project/communityservice/domain/board/post/repository/BoardRepository.java
+++ community-service/src/main/java/project/communityservice/domain/board/post/repository/BoardRepository.java
@@ -1,20 +1,12 @@
 package project.communityservice.domain.board.post.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import project.communityservice.domain.board.post.entity.Board;
 import project.communityservice.domain.board.post.exception.BoardException;
 import project.communityservice.global.exception.ErrorCode;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
     default Board getByIdOrElseThrow(Long id) {
         return findById(id).orElseThrow(() -> new BoardException(ErrorCode.NOT_FOUND_BOARD));
     }
-
-    @Query("SELECT b FROM Board b JOIN b.boardTag t WHERE t.id = :id")
-    Page<Board> findAllByTag(@Param("id") Long id, Pageable pageable);
-
 }
--- community-service/src/main/java/project/communityservice/domain/board/post/repository/BoardRepositoryCustomImpl.java
+++ community-service/src/main/java/project/communityservice/domain/board/post/repository/BoardRepositoryCustomImpl.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import project.communityservice.domain.board.post.entity.Board;
 import project.communityservice.domain.board.post.entity.QBoard;
+import project.communityservice.domain.board.tag.entity.QBoardTag;
 
 import java.util.List;
 
@@ -23,21 +24,14 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom {
 
         // 기본 쿼리
         List<Board> boards = queryFactory
-                .selectFrom(board)
+                .select(board)
+                .from(board)
                 .join(board.boardTag, boardTag)
                 .where(boardTag.id.eq(id))
                 .offset(pageable.getOffset())  // 페이지 시작 위치
                 .limit(pageable.getPageSize()) // 한 페이지에 표시할 개수
                 .fetch();
 
-        // 전체 개수 조회 (페이징 처리를 위해 필요)
-        long total = queryFactory
-                .select(board.count())
-                .from(board)
-                .join(board.boardTag, boardTag)
-                .where(boardTag.id.eq(id))
-                .fetchOne();
-
-        return new PageImpl<>(boards, pageable, total);
+        return new PageImpl<>(boards);
     }
 }
